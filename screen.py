afcc0cbb22a0ad67543685b0547301460a5ca499
gfigueiro23 <gustavopfigueiro@gmail.com>
2021-07-28 15:33:43 -0300

    Versão com interface gráfica

--- screen.py
+++ screen.py
@@ -3,11 +3,14 @@ from PySimpleGUI.PySimpleGUI import WINDOW_CLOSED
 
 def tela():
     sg.theme('Reddit')
-    layout =  [[sg.Text('hello')]]
-    window = sg.Window('Crawler focado', layout=layout, finalize=True, size=(500,300))
+    layout =  [[sg.Text('Digite seu texto')], [sg.InputText(key='texto'), sg.Button('Pesquisar',size=(8,1))]]
+    window = sg.Window('Crawler focado', layout=layout, finalize=True, size=(500,300), text_justification='center', element_justification='center')
     while True:
         event, values = window.read()
         if event == WINDOW_CLOSED:
             break
+
+        if event == 'Pesquisar':
+            print(values['texto'])
 
 tela()
