38f4502134c317540e47f9edc4f823376ce3eb34
gfigueiro23 <gustavopfigueiro@gmail.com>
2021-07-28 15:37:46 -0300

    versao3

--- index.py
+++ index.py
@@ -1,27 +1,47 @@
+from PySimpleGUI.PySimpleGUI import WINDOW_CLOSED
 from navegar import Navegar
 from bs4 import BeautifulSoup
 from unidecode import unidecode
+import PySimpleGUI as sg
 
 from urls import CadastrarTopico, CadastrarUrl, IdentificarTopico, Urlss
 
+def tela():
+    resultado = ''
+    sg.theme('Reddit')
+    layout =  [[sg.Text('Digite seu texto')], [sg.InputText(key='texto'), sg.Button('Pesquisar',size=(8,1))], 
+    [sg.Text(key='produto', size=(50, 100))], [sg.Text(key='price', size=(100, 100))], [sg.Text(key='link', size=(100,100))]]
+    window = sg.Window('Crawler focado', layout=layout, finalize=True, size=(500,300), text_justification='center', element_justification='center', auto_size_text=True)
+    while True:
+        event, values = window.read()
+        if event == WINDOW_CLOSED:
+            break
+
+        if event == 'Pesquisar':
+            text = values['texto']
+            # separação de palavras desse texto
+            text = text.split(" ")
+        
+            urls = Urlss()
+            # chama a classe Urlss
+
+            # chama a classe que identifica o topico
+            identificar = IdentificarTopico(text)
+            lista_urls = urls.getUrls(text) # pega a url de acordo com o texto
+            
+            text = ' '.join(text)
+            details = Navegar(lista_urls[0], str(text)).getItem() # detalhes do item
+            price = details[list(details.keys())[0]]['price'] # preço do item
+            link = details[list(details.keys())[0]]['link'] # link do item
+            produto = list(details.keys())[0]
+            produto = list(details.keys())[0]
+            window['produto'].update(f'Produto: {produto}')
+            window['price'].update(f'Preço: R$ {price}')
+            window['link'].update(f'Link: {link}')
+            
+
 
 if __name__ == '__main__':
-    text = input('Digite um assunto: ')
-
-    text = text.split(" ")
-  
-    urls = Urlss()
+    tela()
 
 
-    
-    identificar = IdentificarTopico(text)
-    lista_urls = urls.getUrls(text)
-    
-    text = ' '.join(text)
-    details = Navegar(lista_urls[0], str(text)).getItem()
-    price = details[list(details.keys())[0]]['price']
-    link = details[list(details.keys())[0]]['link']
-    produto = list(details.keys())[0]
-    print(f"---------- PRODUTO ---------- \n"
-    f"PREÇO: {price} \n"
-    f"LINK: {link}")
--- urls.py
+++ urls.py
@@ -3,76 +3,84 @@ import json
 from unidecode import unidecode
 from io import StringIO
 
+# Cadastra a url 
 class CadastrarUrl():
     def __init__(self, urls):
         self.url = urls
 
+# Classe que cadastra topicos
 class CadastrarTopico():
     def __init__(self, topicos, keywords):
-        self.topicos =  topicos
-        self.keywords = keywords
+        self.topicos =  topicos # topico
+        self.keywords = keywords # palavras chaves do topico
         self.keys = {
             self.topicos : self.keywords
         }
-    def getDict (self):
+    def getDict (self): # retorna a relação das palavras chaves com os topicos
         return self.keys
     
-    def Salvar(self):
+    def Salvar(self): # salva essa relação em uma planilha
         self.json = json.dumps(self.keys)
         df = pd.read_json(self.json)
         df.reset_index(inplace=True, drop=True)
         df.to_excel('topicos.xlsx', index=False)
 
+# Classe que identifica o topico de acordo
+# com as palavras chaves digitadas
 class IdentificarTopico():
     def __init__(self, text):
-        self.dfe = pd.read_excel('topicos.xlsx')
-        self.text = text
+        self.dfe = pd.read_excel('topicos.xlsx') # lê a planilha gerada na classe anterior
+        self.text = text # Texto difitado pelo usuario
     
     def getTopico(self):
-        topico = ''
-        for x in self.text: self.text[self.text.index(x)] = unidecode(x).lower()
-        self.text = ' '.join(self.text)
-        for x in self.dfe.keys():
-            for y in self.dfe[x]:
-                y = unidecode(str(y)).lower()
-                if y in self.text:
-                    topico = x
-        if topico == '':
-            topico = self.text
+        topico = '' # por padrão, topico é vazio
+        for x in self.text: self.text[self.text.index(x)] = unidecode(x).lower() # deixa o texto em lowercase
+        self.text = ' '.join(self.text) # transforma o texto que é uma lista, em uma string
+        for x in self.dfe.keys(): # pra cada topico dentro da planilha:
+            for y in self.dfe[x]: # pra cada palavra chave dentro do topico:
+                y = unidecode(str(y)).lower() # palavra chave em lowercase para evitar conflito
+                if y in self.text: # verifica se existe o texto digitado nas palavras chaves
+                    topico = x # topico recebe o topico do texto digitado 
+        if topico == '': # se o topico for vazio:
+            topico = self.text # topico = texto digitado pelo usuario
         return topico                    
         
 
 class Urlss():
     def __init__(self):
         pass
+
+    # Função que cadastra url
     def CadastrarUrl(self, urls,  topico):
         self.url = urls
 
+
         self.file = {
-            topico: self.url
+            topico: self.url    # dict com a relação dos tópicos com as suas urls
         }
-        self.json2 = json.dumps(self.file)
-        df = pd.read_json(StringIO(self.json2))
-        df.reset_index(inplace=True, drop=True)
-        df.to_excel('urls.xlsx', index=False)
+        self.json2 = json.dumps(self.file) # gera um arquivo json 
+        df = pd.read_json(StringIO(self.json2)) # le o json
+        df.reset_index(inplace=True, drop=True) 
+        df.to_excel('urls.xlsx', index=False) # transforma o json em uma planilha excel
     
+    # Função que pega a url 
     def getUrls(self, text):
-        self.text =  text
-        teste = ' '.join(self.text)
-        if teste != IdentificarTopico(self.text).getTopico():
+        self.text =  text # texto digitado pelo usuário
+        teste = ' '.join(self.text) # transforma o texto digitado em uma string e armazena em uma variavel diferente
+        if teste != IdentificarTopico(self.text).getTopico(): # Se o texto digitado NÃO é igual ao topico retornado:
             self.text =  text
             teste = ' '.join(self.text)
-            self.topico = IdentificarTopico(self.text).getTopico()
-            urlsexcel = pd.read_excel('urls.xlsx')
-            self.topico = self.topico.lower()
+            self.topico = IdentificarTopico(self.text).getTopico() # topico
+            urlsexcel = pd.read_excel('urls.xlsx') # le a planilha de urls gerada anteriormente
+            self.topico = self.topico.lower() # topico em lowercase
             list_urls = []
-            if self.topico in urlsexcel:
-                for x in urlsexcel[self.topico]:
-                    list_urls.append(x)            
+            if self.topico in urlsexcel: # Se existir o topico na planilha
+                for x in urlsexcel[self.topico]: # para cada url dentro desse topico:
+                    list_urls.append(x) # lista de urls + url do topico        
                     
             return list_urls
-        else:
-            list_urls = ['https://www.americanas.com.br/']
+        else: # Caso o texto digitado, não possuí um topico:
+            list_urls = ['https://www.americanas.com.br/'] # url padrão de pesquisa
             return list_urls
             
         
